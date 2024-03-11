365a39f6c6b0e3d1fd0dceaeb5195d8c4410dab8
akshaygp18 <akshaygp41@gmail.com>
2024-03-11 12:49:42 +0530

    tools list added

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from llama_index.query_engine import PandasQueryEngine
-from prompts import instruction_str, new_prompt
+from prompts import instruction_str, new_prompt, context
+from note_engine import note_engine
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent import ReActAgent
+from llama_index.llms import OpenAI
 
 load_dotenv()
 
@@ -12,4 +16,21 @@ population_df = pd.read_csv(population_path)
 population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
 
 population_query_engine.update_prompts({"pandas_prompt": new_prompt})
-population_query_engine.query("What is the population of India in 2022?")
+
+
+tools = [
+    note_engine,
+    QueryEngineTool(query_engine=population_query_engine, metadata=ToolMetadata(
+        name="population_data",
+        description="this gives information at the world population and demograhics",
+        ),
+    ),
+]
+
+llm = OpenAI(model="gpt-3.5-turbo-0613")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
